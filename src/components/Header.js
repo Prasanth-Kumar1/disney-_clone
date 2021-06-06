660ce9b898a8a47ca0c59e796dd91fd046fdcc4b
prasanth-kumar1 <prasanth.kumar@iiitb.org>
2021-06-06 15:52:23 +0530

    Added Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <Nav>
+            Header
+        </Nav>
+    )
+}
+
+export default Header
+
+const Nav = styled.div`
+    height: 70px;
+    background: #090b13;
+`
