3a77659e293ed43964af5af63f0caaef0856bb01
prasanth-kumar1 <prasanth.kumar@iiitb.org>
2021-06-06 17:16:59 +0530

    Added Links In Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,74 @@ import styled from 'styled-components';
 function Header() {
     return (
         <Nav>
-            Header
+            <Logo src="/images/logo.svg"/>
+            <NavMenu>
+                <a>
+                    <img src="/images/home-icon.svg" alt=""/>
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src="/images/search-icon.svg" alt=""/>
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="/images/watchlist-icon.svg" alt=""/>
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="/images/original-icon.svg" alt=""/>
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg" alt=""/>
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg" alt=""/>
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
+            <UserImg src="https://avatars.githubusercontent.com/u/60217440?s=400&u=babc29fece722b2a39f7146abaf3f7107bb84246&v=4"/>
         </Nav>
     )
 }
 
 export default Header
 
-const Nav = styled.div`
+const Nav = styled.nav`
     height: 70px;
     background: #090b13;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
+`
+const Logo = styled.img`
+    width: 80px;
+`
+const NavMenu = styled.div`
+    display: flex;
+    flex: 1;
+    margin-left: 25px;
+    align-items: center;
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+        cursor: pointer;
+        img {
+            height: 20px;
+        }
+
+        span { 
+            font-size: 13px;
+            letter-spacing: 1.42px;
+        }
+    }
+`
+const UserImg = styled.img`
+   width: 48px;
+   height: 48px;
+   border-radius: 50%;
+   cursor: pointer;
 `
