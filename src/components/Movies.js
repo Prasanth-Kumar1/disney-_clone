32a7057fe74b6f5af9e75f427ce0ba17c9026833
prasanth-kumar1 <prasanth.kumar@iiitb.org>
2021-06-07 11:20:38 +0530

    Added Viewer Component

---
+++ src/components/Movies.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Movies() {
+    return (
+        <Container>
+            Recommended For You
+        </Container>
+    )
+}
+
+export default Movies
+
+const Container = styled.div`
+    margin-top: 40px
+`
